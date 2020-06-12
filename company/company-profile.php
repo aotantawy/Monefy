ea80946ee084e542298961868d21826bfd87c3b9
Abd Alrhman Allam <62191765+AbdAlrhmanAllam98@users.noreply.github.com>
2020-06-12 05:00:42 +0200

    declaration of the apis responses

--- company/company-profile.php
+++ company/company-profile.php
@@ -1,6 +1,50 @@
-<?php include "../partials/header.php" ?>
-<?php include "../partials/navbar.php" ?>
+<?php
+function initCallingAPI()
+{
+    
+    $id=11824;
+    $response = file_get_contents('https://www.themuse.com/api/public/companies/'.$id);
+    $response = json_decode($response, true);
 
+    return $response;
+}
+function convertUTCToLocalTime($orgDate)
+{
+    $date   = new DateTime($orgDate,new DateTimeZone("+2:00"));
+    $newDate= $date->format("d-M-Y");
+    return $newDate;
+}
+
+$result=initCallingAPI();
+
+$desciption=$result['description'];
+$nameOfCompany=$result['name'];
+$locations=$result['locations'];
+$numOfLocations=sizeof($locations);
+foreach($locations as $location)
+{
+  echo $location['name']."<br>";
+}
+$industries=$result['industries'];
+foreach($industries as $industry)
+{
+  echo $industry['name']."<br>";
+}
+
+$publication_date=convertUTCToLocalTime($result['publication_date']);
+
+$id=$result['id'];
+$sizeOfCompany=$result['size']['short_name'];
+
+//urls
+$logoImage=$result['refs']['logo_image'];
+$f1Image=$result['refs']['f1_image'];
+$f2Image=$result['refs']['f2_image'];
+$f3Image=$result['refs']['f3_image'];
+echo "<br>"."<br>";
+
+
+/*
 <div class="jumbotron container-fluid">
   <div class="company-intro">
     <img src="../assets/img/google-image.png" alt="goolge image" class="float-left company-image">
@@ -33,5 +77,5 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
   </a>
 </div>
-
-<?php include "../partials/footer.php" ?>
+*/
+?>
