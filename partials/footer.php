8e361a0ac0f354cec8938b66eab0d1aa8c82649b
ahmedosama9100 <ahmedosama9200@gmail.com>
2020-06-12 10:45:27 +0200

    modify partials/footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -16,7 +16,11 @@
 
 <!-- Template Main JS File -->
 <script src="/stock/assets/js/home/home.js"></script>
- <!-- Register-->
- <script src="/stock/register/js/registering.min.js"></script>
-  <!-- news-page-->
-  <script src="/stock/news-page/js/index.js"></script>
+<!-- Register-->
+<script src="/stock/register/js/registering.min.js"></script>
+<!-- news-page-->
+<script src="/stock/news-page/js/index.js"></script>
+
+</body>
+
+</html>
